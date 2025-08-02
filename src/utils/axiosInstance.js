78f416d36e5e20feb6213ff17cb6e1fb261d0651
Mayank Satish Lakkewar <mayanklakkewar01@gmail.com>
2025-08-03 00:09:46 +0530

    Update axiosInstance.js

--- src/utils/axiosInstance.js
+++ src/utils/axiosInstance.js
@@ -1,7 +1,5 @@
 import axios from "axios";
 import { BASE_URL } from "./apiPaths";
-console.log("API base URL:", import.meta.env.VITE_API_BASE_URL);
-
 
 const axiosInstance = axios.create({
   baseURL: BASE_URL,
